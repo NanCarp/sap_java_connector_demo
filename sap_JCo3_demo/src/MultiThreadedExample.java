01c9baa3871616a8d08d432eddcdc4e22012dc74
NanCarp <nancarpli@163.com>
2017-05-05 08:21:38 +0800

    与SAP连接配置

--- sap_JCo3_demo/src/MultiThreadedExample.java
+++ sap_JCo3_demo/src/MultiThreadedExample.java
@@ -1,12 +1,16 @@
 import com.sap.conn.jco.*;
-import com.sap.conn.jco.ext.DestinationDataProvider;
-import com.sap.conn.jco.ext.JCoSessionReference;
-import com.sap.conn.jco.ext.SessionReferenceProvider;
+import com.sap.conn.jco.ext.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Collection;
+import java.util.Hashtable;
 import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -140,22 +144,152 @@ public class MultiThreadedExample {
             }
             super.cleanUp();
         }
-
-        /*static class MySessionReferenceProvider implements SessionReferenceProvider {
-            public JCoSessionReference getCurrentSessionReference(String scopeType) {
-                MySessionReference
-            }
-        }
-
-        static class MySessionReference implements JCoSessionReference {
-
-        }*/
-
-
     }
 
+    static class MySessionReferenceProvider implements SessionReferenceProvider {
+        public JCoSessionReference getCurrentSessionReference(String scopeType) {
+            MySessionReference sesRef = WorkerThread.localSessionReference.get();
+            if (sesRef != null) {
+                return sesRef;
+            }
+
+            throw new RuntimeException("Unknown thread:" + Thread.currentThread().getId());
+        }
+
+        public boolean isSessionAlive(String sessionId) {
+            Collection<MySessionReference> availableSessions = WorkerThread.sessions.values();
+            for (MySessionReference ref : availableSessions) {
+                if (ref.getID().equals(sessionId)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void jcoServerSessionContinued(String sessionID) throws SessionException {
+        }
+        public void jcoServerSessionFinished(String sessionID) {
+        }
+        public void jcoServerSessionPassivated(String sessionID) throws SessionException {
+        }
+        public JCoSessionReference jcoServerSessionStarted() throws SessionException {
+            return null;
+        }
+    }
+
+    static class MySessionReference implements JCoSessionReference {
+        static AtomicInteger atomicInt = new AtomicInteger(0);
+        private String id = "session-" + String.valueOf(atomicInt.addAndGet(1));
+
+        public void contextFinished() {
+
+        }
+
+        public void contextStarted() {
+
+        }
+
+        public String getID() {
+            return id;
+        }
+    }
+
+    static class WorkerThread extends Thread {
+        static Hashtable<MultiStepJob, MySessionReference> sessions = new Hashtable<MultiStepJob, MySessionReference>();
+        static ThreadLocal<MySessionReference> localSessionReference = new ThreadLocal<MySessionReference>();
+
+        private CountDownLatch doneSignal;
+
+        WorkerThread(CountDownLatch doneSignal) {
+            this.doneSignal = doneSignal;
+        }
+
+        @Override
+        public void run() {
+            try{
+                for(;;) {
+                    MultiStepJob job = queue.poll(10, TimeUnit.SECONDS);
+
+                    //  stop if nothing to do
+                    if (job == null) {
+                        return;
+                    }
+
+                    MySessionReference sesRef = sessions.get(job);
+                    if (sesRef == null) {
+                        sesRef = new MySessionReference();
+                        sessions.put(job, sesRef);
+                    }
+                    localSessionReference.set(sesRef);
+
+                    System.out.println("Task " + job.getName() + " is started.");
+                    try {
+                        job.runNextStep();
+                    } catch (Throwable th) {
+                        th.printStackTrace();
+                    }
+
+                    if (job.isFinished()) {
+                        System.out.println("Task " + job.getName() + " is finished.");
+                        sessions.remove(job);
+                        job.cleanUp();
+                    } else {
+                        System.out.println("Task " + job.getName() + " is passivated.");
+                        queue.add(job);
+                    }
+                    localSessionReference.set(null);
+                }
+            } catch (InterruptedException e) {
+                // just leave
+            } finally {
+                doneSignal.countDown();
+            }
+        }
+    }
+
+    private static BlockingQueue<MultiStepJob> queue = new LinkedBlockingQueue<MultiStepJob>();
     private static JCoFunctionTemplate incrementCounterTemplate, getCounterTemplate;
+
+    static void runJobs(JCoDestination destination, int jobCount, int threadCount) {
+        System.out.println(">>> Start");
+        for(int i = 0; i < jobCount; i++) {
+            queue.add(new StatelessMultiStepExample(destination, 10));
+            queue.add(new StatefulMultiStepExample(destination, 10));
+        }
+
+        CountDownLatch doneSignal = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            new WorkerThread(doneSignal).start();
+        }
+
+        System.out.print(">>> Wait ...");
+        try {
+            doneSignal.await();
+        } catch (InterruptedException ie) {
+            //
+        }
+        System.out.println(">>> Done");
+    }
+
+    public static void main(String[] argv) {
+        Environment.registerSessionReferenceProvider(new MySessionReferenceProvider());
+        try {
+            JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+            incrementCounterTemplate = destination.getRepository().getFunctionTemplate("Z_INCREMENT_COUNTER");
+            getCounterTemplate = destination.getRepository().getFunctionTemplate("Z_GET_COUNTER");
+            if (incrementCounterTemplate == null || getCounterTemplate == null) {
+                throw new RuntimeException("This example cannot run without Z_INCREMENT_COUNTER and Z_GET_COUNTER functions");
+            }
+            runJobs(destination, 5, 2);
+        } catch (JCoException je) {
+            je.printStackTrace();
+        }
+    }
+
 }
+
+
+
 
 
 
--- sap_JCo3_demo/src/StepByStepClient.java
+++ sap_JCo3_demo/src/StepByStepClient.java
@@ -1,3 +1,4 @@
+import com.sap.conn.jco.*;
 import com.sap.conn.jco.ext.DestinationDataProvider;
 
 import java.io.File;
@@ -35,4 +36,139 @@ public class StepByStepClient {
             throw new RuntimeException("Unable to create the destination files", e);
         }
     }
+
+    public static void step1Connect() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME1);
+        System.out.println("Attributes:");
+        System.out.println(destination.getAttributes());
+        System.out.println();
+    }
+
+    public static void step2ConnectUsingPool() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        destination.ping();
+        System.out.println("Attributes:");
+        System.out.println(destination.getAttributes());
+        System.out.println();
+    }
+
+    public static void step3SimpleCall() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        JCoFunction function = destination.getRepository().getFunction("STFC_CONNECTION");
+        if (function == null) {
+            throw new RuntimeException("BAPI_COMPANYCODE_GETLIST not found in SAP.");
+        }
+
+        function.getImportParameterList().setValue("REQUTEXT", "Hello SAP");
+
+        try {
+            function.execute(destination);
+        } catch (AbapException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        System.out.println("STFC_CONNECTION finished:");
+        System.out.println(" Echo: " + function.getExportParameterList().getString("ECHOTEXT"));
+        System.out.println(" Response: " + function.getExportParameterList().getString("RESPTEXT"));
+        System.out.println();
+    }
+
+    public static void step3WorkWithStructure() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        JCoFunction function = destination.getRepository().getFunction("RFC_SYSTEM_INFO");
+        if (function == null) {
+            throw new RuntimeException("BAPI_COMPANYCODE_GETLIST not found in SAP.");
+        }
+
+        try {
+            function.execute(destination);
+        } catch (AbapException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        JCoStructure exportStructure = function.getExportParameterList().getStructure("RFCSI_EXPORT");
+        System.out.println("System info for " + destination.getAttributes().getSystemID() + ":\n");
+        for(int i = 0; i < exportStructure.getMetaData().getFieldCount(); i++) {
+            System.out.println(exportStructure.getMetaData().getName(i) + ":\t" + exportStructure.getString(i));
+        }
+
+        System.out.println();
+    }
+
+    public static void step4WorkWithTable() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        JCoFunction function = destination.getRepository().getFunction("RFC_SYSTEM_INFO");
+        if (function == null) {
+            throw new RuntimeException("BAPI_COMPANYCODE_GETLIST not found in SAP.");
+        }
+
+        try {
+            function.execute(destination);
+        } catch (AbapException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        JCoStructure returnStructure = function.getExportParameterList().getStructure("RETURN");
+        if (!(returnStructure.getString("TYPE").equals("") || returnStructure.getString("TYPE").equals("S"))) {
+            throw new RuntimeException(returnStructure.getString("MESSAGE"));
+        }
+        JCoTable codes = function.getTableParameterList().getTable("COMPANYCODE_LIST");
+        for (int i = 0; i < codes.getNumRows(); i++) {
+            codes.setRow(i);
+            System.out.println(codes.getString("COMP_CODE") + '\t' + codes.getString("COM_NAME"));
+        }
+
+        codes.firstRow();
+        for (int i = 0; i < codes.getNumRows(); i++, codes.nextRow()) {
+            function = destination.getRepository().getFunction("BAPI_COMPANYCODE_GETDETAIL");
+            if (function == null) {
+                throw new RuntimeException("BAPI_COMPANYCODE_GETDETAIL not found in SAP.");
+            }
+
+            function.getImportParameterList().setValue("COMPANYCODEID", codes.getString("COMP_CODE"));
+
+            function.getExportParameterList().setActive("COMPANY_CODE", false);
+
+            try {
+                function.execute(destination);
+            } catch (AbapException e) {
+                System.out.println(e.toString());
+                return;
+            }
+
+            returnStructure = function.getExportParameterList().getStructure("return");
+            if (!(returnStructure.getString("TYPE").equals("") ||
+                  returnStructure.getString("TYPE").equals("S") ||
+                  returnStructure.getString("TYPE").equals("W"))) {
+                throw new RuntimeException(returnStructure.getString("MESSAGE"));
+            }
+
+            JCoStructure detail = function.getExportParameterList().getStructure("COMPANY_DETAIL");
+            System.out.println(detail.getString("COMP_CODE") + '\t' + detail.getString("COUNTRY") + '\t' +
+                                                detail.getString("CITY"));
+        }
+
+
+
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
